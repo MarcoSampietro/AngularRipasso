fe8c5c65d18a34eaba52da70c6f176234d8c858f
MarcoSampietro <166590850+MarcoSampietro@users.noreply.github.com>
2025-10-19 13:48:25 +0000

    ripasso

--- ripasso/src/app/app.ts
+++ ripasso/src/app/app.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,4 +14,38 @@ export class App {
   evento(){
     this.messaggio = 'Evento andato a buon fine'
   }
-}
+  messaggiobool = true;
+  mostramessaggio(){
+    this.messaggiobool = !this.messaggiobool;
+  }
+  frutti = ['Mela', 'Pera', 'Banana', 'Arancia', 'Ananas'];
+  rimuoviFrutto(index:number){//rimuove un elemento dalla lista
+    this.frutti.splice(index, 1)
+  }
+  aggiungiFrutto(nuovo: string) {
+    const nome = nuovo.trim();
+    if (nome) {
+    this.frutti.push(nome);
+    }
+  }
+
+  evidenzia = false;
+  toggleEvidenzia() {
+    this.evidenzia = !this.evidenzia;
+  }
+
+  dimensione = 16;
+  colore = 'black';
+
+  aumenta() {
+    this.dimensione += 2;
+  }
+  diminuisci() {
+    if (this.dimensione > 8) {
+    this.dimensione -= 2;
+  }
+  }
+  cambiaColore(nuovoColore: string) {
+    this.colore = nuovoColore;
+  }
+  }
